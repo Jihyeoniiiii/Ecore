fc32d92c85b2593ea65e4eb649278225a9edb6c3
jihyeoniiiii <qwere4585@naver.com>
2024-10-24 20:40:55 +0900

    feat: home banner logo change

--- lib/home_page/home_page_banner.dart
+++ lib/home_page/home_page_banner.dart
@@ -69,10 +69,10 @@ class _TitleBannerState extends State<TitleBanner> {
         title: Row(
           children: [
             Image.asset(
-              'assets/images/ecore_logo.png', // 로고 이미지 경로
-              height: 40, // 로고의 높이를 설정 (필요에 따라 크기 조정)
+              'assets/images/logo.png',
+              height: 40,
             ),
-            Spacer(), // 텍스트와 검색 아이콘 사이의 공간 확보
+            Spacer(),
             IconButton(
               onPressed: () {
                 Navigator.push(
